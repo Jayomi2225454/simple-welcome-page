5c8a92cae3a477eaf452e642661ab50bec1b1c22
Jayomi2225454 <kathanproject@gmail.com>
2026-01-31 23:02:49 +0530

    Add files via upload

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import AIChatbot from '@/components/chat/AIChatbot';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen bg-gray-900 flex flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+      <AIChatbot />
+    </div>
+  );
+};
+
+export default Layout;
